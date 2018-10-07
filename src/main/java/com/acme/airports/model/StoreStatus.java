2a2147a378c5624c12c24a29b87315f087ed4495
kivancsahici <kivancsahici@yahoo.com>
2018-10-07 23:09:38 +0300

    display open, closing_soon and closed status

--- src/main/java/com/acme/airports/model/StoreStatus.java
+++ src/main/java/com/acme/airports/model/StoreStatus.java
@@ -1,5 +1,12 @@
 package com.acme.airports.model;
 
+import com.fasterxml.jackson.annotation.JsonValue; 
+
 public enum StoreStatus {
-	OPEN, CLOSING_SOON, CLOSED
+	OPEN, CLOSING_SOON, CLOSED;
+	
+	@JsonValue
+	public String toJson() {
+		return name().toLowerCase();
+	}
 }
