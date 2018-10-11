e681fd3f89bd199f3b2bccbc1bd5fbd32ea7bda0
kivancsahici <kivancsahici@yahoo.com>
2018-10-11 16:52:02 +0300

    temp fix unit tests

--- src/test/java/com/acme/airports/test/service/StoreRepositoryIntegrationTest.java
+++ src/test/java/com/acme/airports/test/service/StoreRepositoryIntegrationTest.java
@@ -29,19 +29,18 @@ public class StoreRepositoryIntegrationTest {
     // write test cases here
     @Test
     public void findNearestStores() {
-    	
+    	/*
     	List<Store> retval = storeRepo.findByCityOrderByStreet("Alkmaar");
     	for(Store store : retval) {
     		System.out.println(store.getAddressName());
-    	}/*
-    	List<StoreResult> storeResult = storeRepo.findNearestStores(52.075854, 4.234678, null, null);    	
+    	}*/
+    	List<StoreResult> storeResult = storeRepo.findNearestStores(52.075854, 4.234678, 25, 5);    	
         assertThat(storeResult, hasSize(5));
         
-        storeResult = storeRepo.findNearestStores(51.497644, 7.393050, null, null);
+        storeResult = storeRepo.findNearestStores(51.497644, 7.393050,  25, 5);
         assertThat(storeResult, hasSize(0));
         
-        storeResult = storeRepo.findNearestStores(51.823928, 6.735862, null, null);
+        storeResult = storeRepo.findNearestStores(51.823928, 6.735862,  25, 5);
         assertThat(storeResult, hasSize(4));
-    	*/
     }
 }
