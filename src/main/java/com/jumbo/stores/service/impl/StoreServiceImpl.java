3607f1dd6b984107d9ed770f737e327e995258da
kivancsahici@yahoo.com <kivancsahici@yahoo.com>
2019-09-07 11:33:52 +0200

    avoid NullPointerException

--- src/main/java/com/jumbo/stores/service/impl/StoreServiceImpl.java
+++ src/main/java/com/jumbo/stores/service/impl/StoreServiceImpl.java
@@ -5,6 +5,7 @@ import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,26 +19,26 @@ import com.jumbo.stores.service.dto.NearestStores;
 @Service
 public class StoreServiceImpl implements IStoreService{
 	@Autowired
-	IStoreRepository storeRepository;	
-	
+	IStoreRepository storeRepository;
+
 	@Override
-	public List<Store> saveAll(List<Store> storeList) {				
-		storeList.removeIf(s -> s.getTodayClose().equals("Gesloten"));
+	public List<Store> saveAll(List<Store> storeList) {
+		storeList.removeIf(s -> s.getTodayClose() != null && s.getTodayClose().equals("Gesloten"));
 		return storeRepository.saveAll(storeList);
 	}
-	
+
 	@Override
 	public List<Store> findByCity(String city) {
 		return storeRepository.findByCityOrderByStreet(city);
 	}
-	
+
 	@Override
 	public NearestStores findByCityAndStreet(String city, String street) {
 		List<Store> storeList = storeRepository.findByCityAndStreetOrderByStreet(city, street);
 		List<StoreResult> storeResultList = new ArrayList<>();
-		
+
 		LocalTime now = LocalTime.now(ZoneId.of("GMT+2"));
-		
+
 		for(Store store : storeList) {
 			StoreResult storeResult = new StoreResult();
 			storeResult.setAddressName(store.getAddressName());
@@ -48,7 +49,7 @@ public class StoreServiceImpl implements IStoreService{
 			storeResult.setSapStoreID(store.getSapStoreID());
 			storeResult.setTodayOpen(store.getTodayOpen());
 			storeResult.setTodayClose(store.getTodayClose());
-			
+
 			LocalTime from = LocalTime.parse(storeResult.getTodayOpen());
 			LocalTime to = LocalTime.parse(storeResult.getTodayClose());
 			if(now.isAfter(from) && now.isBefore(to)) {
@@ -65,15 +66,16 @@ public class StoreServiceImpl implements IStoreService{
 		stores.setNearestStores(storeResultList);
 		return stores;
 	}
-	
+
 	@Override
-	public NearestStores findNearestStores(Double latitude, Double longitude, Integer radius, Integer maxResult, Boolean showOpen) {		
+	public NearestStores findNearestStores(Double latitude, Double longitude, Integer radius, Integer maxResult,
+			Boolean showOpen) {
 		List<StoreResult> storeList = storeRepository.findNearestStores(latitude, longitude, radius, maxResult);
-						
+
 		LocalTime now = LocalTime.now(ZoneId.of("GMT+2"));
-		
+
 		for(StoreResult result : storeList) {
-						
+
 			LocalTime from = LocalTime.parse(result.getTodayOpen());
 			LocalTime to = LocalTime.parse(result.getTodayClose());
 			if(now.isAfter(from) && now.isBefore(to)) {
@@ -87,14 +89,14 @@ public class StoreServiceImpl implements IStoreService{
 		}
 		if(showOpen)
 			storeList.removeIf(s -> s.getStoreStatus().equals(StoreStatus.CLOSED));
-		
+
 		NearestStores nearestStores = new NearestStores();
 		nearestStores.setNearestStores(storeList);
 		nearestStores.setLatitude(latitude);
 		nearestStores.setLongitude(longitude);
 		return nearestStores;
 	}
-	
+
 	@Override
 	public List<String> findUniqueCities() {
 		return storeRepository.findDistinctCities();
